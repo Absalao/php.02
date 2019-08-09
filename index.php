54dfeafa5b6ecd212941c06e9507af29048bc52a
Absalao Newton <absalao.new@gmail.com>
2019-08-08 21:56:08 -0300

    Criando Pag Cadastro 08/08/2019

--- index.php
+++ index.php
@@ -2,11 +2,31 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Well.com</title>
+   
 </head>
 <body>
-
-   <h1> Hello,world</h1>
-    
+   
+   
+   
+    <div class="card card-body bg-dark">
+        <h3 class="text-white">Projeto de Controle de Clientes</h3>
+        <p class="text-white">
+            Sistema desenvolvido em PHP e MySQL
+        </p>
+    </div>
+    <br>
+    <div class="container">
+        <h2>Painel do Sistema</h2>
+        <hr>
+        <h4>Menu</h4>
+        <nav>
+            <ul>
+                <li><a href="cadastrar.php">Cadastro de Clientes</a></li>
+                <li><a href="consultar.php">Consulta de Clientes</a></li>
+            </ul>
+        </nav>
+    </div>
 </body>
 </html>
